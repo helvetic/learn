f92608b2407a57ab4b5e173ce7fb0baa87105910
helvetic <my@maggit.ru>
2016-08-02 14:06:14 +0300

    Функция переделана в класс
    Функция переделана в класс для облегчения работы с модулем cookie

--- src/modules/dragNDrop.js
+++ src/modules/dragNDrop.js
@@ -1,31 +1,44 @@
-
 /**
  * Создать страницу с кнопкой. При клике на кнопку, на странице должен создаваться div произвольных размеров, в произвольном месте.
  * Цвет фона div'а должен быть каждый раз случайным.
  * Созданные div'ы можно перетаскивать мышкой (drag & drop)
  * @param  {string} btn - кнопка, на которую вешается событие click
  */
-export default function (btn) {
+export class Dnd{
 
-  let highestSize = 400,
-      lowestSize = 10,
-      bodyHeight = document.body.clientHeight,
-      bodyWidth = document.body.clientWidth;
+  constructor(btn){
+    this.highestSize = 400;
+    this.lowestSize = 50;
+    this.bodyHeight = document.body.clientHeight;
+    this.bodyWidth = document.body.clientWidth;
+    this.squaresData = [];
 
+    document.querySelector(btn).addEventListener('click', () => {
+      let {highestSize,lowestSize,bodyHeight,bodyWidth} = this;
+      let height = randomNumber(highestSize, lowestSize),
+          width = randomNumber(highestSize, lowestSize);
+      let obj = {
+        bg: randomColor(),
+        width: width + "px",
+        height: height + "px",
+        top: randomNumber(0, bodyHeight - height) + "px",
+        left: randomNumber(0, bodyWidth - width) + "px"
+      };
+      this.createSquare(obj);
+    });
+  }
 
-  document.querySelector(btn).addEventListener('click', function(){
+  createSquare(obj){
+    let sq = document.createElement('div');
 
-    let sq = document.createElement('div'),
-        height = randomNumber(highestSize, lowestSize),
-        width = randomNumber(highestSize, lowestSize);
-
+    this.squaresData.push(obj);
 
     sq.className = 'drag-square';
-    sq.style.backgroundColor = randomColor();
-    sq.style.width = width + "px";
-    sq.style.height = height + "px";
-    sq.style.top = randomNumber(0, bodyHeight - height) + "px";
-    sq.style.left = randomNumber(0, bodyWidth - width) + "px";
+    sq.style.backgroundColor = obj.bg;
+    sq.style.width = obj.width;
+    sq.style.height = obj.height;
+    sq.style.top = obj.top;
+    sq.style.left = obj.left;
 
     document.body.appendChild(sq);
 
@@ -38,9 +51,11 @@ export default function (btn) {
       activeElement = e.target;
       offsetX = e.offsetX;
       offsetY = e.offsetY;
+      activeElement.classList.add('active');
     });
 
     sq.addEventListener('mouseup', function(e){
+      activeElement.classList.remove('active');
       activeElement = null;
     });
 
@@ -51,10 +66,9 @@ export default function (btn) {
       }
     });
 
-  });
-
+    return this.squaresData;
+  }
 }
-
 
 function randomColor(){
   return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
@@ -63,3 +77,66 @@ function randomColor(){
 function randomNumber(min, max){
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+// /* old */
+// export default function (btn) {
+//
+//   let highestSize = 400,
+//       lowestSize = 50,
+//       bodyHeight = document.body.clientHeight,
+//       bodyWidth = document.body.clientWidth,
+//       squaresData = [];
+//
+//
+//   document.querySelector(btn).addEventListener('click', function(){
+//
+//     let sq = document.createElement('div'),
+//         height = randomNumber(highestSize, lowestSize),
+//         width = randomNumber(highestSize, lowestSize);
+//
+//     let obj = {
+//       bg: randomColor(),
+//       width: width + "px",
+//       height: height + "px",
+//       top: randomNumber(0, bodyHeight - height) + "px",
+//       left: randomNumber(0, bodyWidth - width) + "px"
+//     };
+//     squaresData.push(obj);
+//
+//     sq.className = 'drag-square';
+//     sq.style.backgroundColor = obj.bg;
+//     sq.style.width = obj.width;
+//     sq.style.height = obj.height;
+//     sq.style.top = obj.top;
+//     sq.style.left = obj.left;
+//
+//     document.body.appendChild(sq);
+//
+//
+//     let activeElement,
+//         offsetX = 0,
+//         offsetY = 0;
+//
+//     sq.addEventListener('mousedown', function(e){
+//       activeElement = e.target;
+//       offsetX = e.offsetX;
+//       offsetY = e.offsetY;
+//       activeElement.classList.add('active');
+//     });
+//
+//     sq.addEventListener('mouseup', function(e){
+//       activeElement.classList.remove('active');
+//       activeElement = null;
+//     });
+//
+//     sq.addEventListener('mousemove', function(e){
+//       if (activeElement != null) {
+//         activeElement.style.top = (e.clientY - offsetY) + 'px';
+//         activeElement.style.left = (e.clientX - offsetX) + 'px';
+//       }
+//     });
+//
+//   });
+//
+//   return squaresData;
+// }
